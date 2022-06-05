f22d6123cd9d853ca3b730dbde9bd0ffda9cc403
Filip Czaplicki <github@starsep.com>
2022-06-05 13:19:14 +0200

    Add AED locations to style

--- src/components/map_style.js
+++ src/components/map_style.js
@@ -13,6 +13,11 @@ export default {
             "minzoom": 0,
             "maxzoom": 19,
             "attribution": "© <a target=\"_blank\" rel=\"noopener\" href=\"https://openstreetmap.org/copyright\">OpenStreetMap contributors</a>"
+        },
+        "aed-locations": {
+          "type": "vector",
+          "tiles": ["https://aed.openstreetmap.org.pl/mvt/{z}/{x}/{y}.pbf"],
+          "maxzoom": 13
         }
     },
     "sprite": "http://localhost:3000/sprite",
@@ -25,6 +30,47 @@ export default {
             "layout": {
                 "visibility": "visible"
             }
+        },
+        {
+            "id": "unclustered",
+            "type": "symbol",
+            "source": "aed-locations",
+            "source-layer": "defibrillators",
+            "filter": ["!", ["has", "point_count"]],
+            "layout": {
+                "icon-image": "marker_default",
+                "icon-size": 0.5,
+                "icon-allow-overlap": true
+            }
+        },
+        {
+          "id": "clustered-circle",
+          "type": "circle",
+          "source": "aed-locations",
+          "source-layer": "defibrillators",
+          "filter": ["has", "point_count"],
+          "layout": {"visibility": "visible"},
+          "paint": {
+            "circle-color": "rgba(0,145,64, 0.85)",
+            "circle-radius": 12,
+            "circle-stroke-color": "rgba(245, 245, 245, 0.88)",
+            "circle-stroke-width": 2
+          }
+        },
+        {
+          "id": "clustered-label",
+          "type": "symbol",
+          "source": "aed-locations",
+          "source-layer": "defibrillators",
+          "filter": ["has", "point_count"],
+          "layout": {
+            "text-field": "{point_count}",
+            "text-font": ["Open Sans Bold"],
+            "text-size": 12,
+            "text-letter-spacing": 0.05,
+            "visibility": "visible"
+          },
+          "paint": {"text-color": "#f5f5f5"}
         }
     ],
     "id": "style"
