6edff2865868c5fa36ca87bb9b09a67005a0e5c7
tomasz t <tomasztaras@outlook.com>
2022-06-10 01:05:40 +0200

    added icon

--- src/components/languageSwitcher.js
+++ src/components/languageSwitcher.js
@@ -2,12 +2,15 @@ import React from 'react';
 import i18n from 'i18next';
 import { languages } from '../i18n';
 import { Navbar } from 'react-bulma-components';
+import Icon from '@mdi/react'
+import { mdiTranslate } from '@mdi/js';
 
 
 export default function LanguageSwitcher(){
     return (
         <Navbar.Item hoverable="true" >
             <Navbar.Link key={i18n.resolvedLanguage}>
+                <Icon path={mdiTranslate} size={1.3} className="icon mr-2" color="#fff" />
                 {languages[i18n.resolvedLanguage].nativeName}
             </Navbar.Link>
             <Navbar.Dropdown>
