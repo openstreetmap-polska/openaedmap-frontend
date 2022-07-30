0855b40dae68d71a3de2c622679538c2efd8a088
tomasz t <tomasztaras@outlook.com>
2022-07-30 15:16:30 +0000

    improved bottom bar

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Button, Columns, Footer } from 'react-bulma-components';
+import { Button, Footer } from 'react-bulma-components';
 import { useTranslation } from 'react-i18next';
 import Icon from '@mdi/react'
 import {mdiMapMarkerPlus} from '@mdi/js'
@@ -11,22 +11,20 @@ export default function FooterDiv() {
     return (
         <Footer className='footer-div'>
             <div className='white-bottom-bar'></div>
-            <Columns pb={0} pt={2} /*className='is-hidden-mobile'*/>
-                <Columns.Column pb={1}>
+            <div className='bottom-bar-buttons'>
+                <div className='is-hidden-mobile'>
+                    <Button color={'success'} mt={1} ml={2} className='has-text-weight-light'>
+                        <Icon path={mdiMapMarkerPlus} className='icon mr-2' />
+                        {t('footer.add_aed')}
+                    </Button>
+                </div>
+                <div className='is-hidden-tablet'>
                     <Button color={'success'} mt={1} ml={2} className='has-text-weight-light'>
                         <Icon path={mdiMapMarkerPlus} className='icon mr-2' />
                         {t('footer.add')}
                     </Button>
-                </Columns.Column>
-            </Columns>
-            {/* <Columns pb={0} pt={2} className='is-hidden-tablet'>
-                <Columns.Column pb={1} narrow={true} ml={2}>
-                    <Button color={'success'} mt={1} ml={2} className='has-text-weight-light'>
-                        <Icon path={mdiMapMarkerPlus} className='icon mr-2' />
-                        {t('footer.add')}
-                    </Button>
-                </Columns.Column>
-            </Columns>*/}
+                </div>
+            </div>
         </Footer>
     )
 }
