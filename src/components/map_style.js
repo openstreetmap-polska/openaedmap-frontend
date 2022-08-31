a332fea33571ce6482365d8455102d07c8bb2ec6
tomasz t <tomasztaras@outlook.com>
2022-08-31 19:56:42 +0000

    changed osm tile url according to their newest recommendation

--- src/components/map_style.js
+++ src/components/map_style.js
@@ -7,11 +7,7 @@ const style = {
     "sources": {
         "osm": {
             "type": "raster",
-            "tiles": [
-                "https://a.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                "https://b.tile.openstreetmap.org/{z}/{x}/{y}.png",
-                "https://c.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            ],
+            "tiles": ["https://tile.openstreetmap.org/{z}/{x}/{y}.png"],
             "tileSize": 256,
             "minzoom": 0,
             "maxzoom": 19,
