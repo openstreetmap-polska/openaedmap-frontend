50b37140d67e8edb66f9e47a49d14ff5874d8b71
coolawik <106607132+Coolawik@users.noreply.github.com>
2022-09-18 12:35:52 +0200

    Update modal.js

--- src/components/modal.js
+++ src/components/modal.js
@@ -40,8 +40,8 @@ const ModalContent = ({ state }) => {
     } else if (state.type === "info") {
         return (
             <div>
+                <p className="pb-2">{t("modal.about_project")}</p>
                 <p className="pb-2">{t("modal.about_osm")}</p>
-                <p className="pb-2">{t("modal.history")}</p>
                 <p>
                     {t("modal.create_account")}
                     <a href="https://osm.org" rel="noreferrer" target="_blank">osm.org</a>
