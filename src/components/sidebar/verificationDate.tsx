2367bbca729cfa1fc8a919de18f14208e9a57fbb
Ancymonek <mac1i1ek@gmail.com>
2022-12-04 20:37:23 +0100

    Check_date - display in sidebar

--- src/components/sidebar/verificationDate.tsx
+++ src/components/sidebar/verificationDate.tsx
@@ -1,11 +1,24 @@
 import { useTranslation } from 'react-i18next';
 import { mdiCalendar } from '@mdi/js';
 import Icon from '@mdi/react'
+import React, {FC} from 'react';
+
+
+export const CheckDateField: FC<CheckDateProps> =({ check_date }) => {
+  const { t } = useTranslation();
+
+  if (check_date) {
+      return <span className='is-pulled-right is-flex has-text-grey-dark has-text-weight-light is-size-7 p-0 is-justify-content-center'><Icon path={mdiCalendar} size={0.7} color='#dbdbdb' className='mr-1'/>{t('sidebar.verification_date') + ": "}<span className="has-text-weight-medium">{check_date}</span></span>
+  } else {
+    return <span className='is-pulled-right is-flex has-text-grey-dark has-text-weight-light is-size-7 p-0 is-justify-content-center'><Icon path={mdiCalendar} size={0.7} color='#dbdbdb' className='mr-1'/>{t('sidebar.verification_date') + ": "}<span className='is-italic has-text-grey-light'>{t("sidebar.no_data")}</span></span>
+  }
+};
 
 export function CheckDateFormField() {
-    const { t } = useTranslation();
     var curr = new Date();
     var date = curr.toISOString().substring(0,10);
+
+    const { t } = useTranslation();
 
     return (
         <div className="field">
@@ -18,4 +31,8 @@ export function CheckDateFormField() {
         </div>
       </div>
     )
+}
+
+interface CheckDateProps {
+  check_date: string,
 }
