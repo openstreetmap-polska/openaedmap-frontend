34ea42e6f7f61424a8893af65697a454da7f2133
tomasz t <tomasztaras@outlook.com>
2022-12-28 22:31:58 +0000

    added link to file download

--- src/components/downloadCard.tsx
+++ src/components/downloadCard.tsx
@@ -1,28 +1,9 @@
-import React from 'react';
 import { useTranslation } from 'react-i18next';
 import Icon from '@mdi/react'
-import { mdiDownload, mdiEarth, mdiFileDelimited, mdiFileExcel } from '@mdi/js';
+import { mdiDownload, mdiEarth } from '@mdi/js';
 
 export default function DownloadCard() {
     const { t } = useTranslation();
-    const dataDomain = "https://aed.openstreetmap.org.pl"; // TODO: change domain, maybe use relative url
-    const downloadFormats = [
-        {
-            'icon': mdiEarth,
-            'name': t('sidebar.geojson'),
-            'url': `${dataDomain}/aed_poland.geojson`
-        },
-        {
-            'icon': mdiFileDelimited,
-            'name': t('sidebar.csv'),
-            'url': `${dataDomain}/aed_poland.csv`
-        },
-        {
-            'icon': mdiFileExcel,
-            'name': t('sidebar.excel'),
-            'url': `${dataDomain}/aed_poland.ods`
-        }
-    ];
 
     return (
         <div className='px-4 pt-5'>
@@ -31,11 +12,9 @@ export default function DownloadCard() {
                     <Icon path={mdiDownload} size={1} className='icon mr-1' color='#7a7a7a' />
                     {t('sidebar.download_title')}
                 </p>
-                {downloadFormats.map(({icon, name, url}) =>
-                    <a className='button is-success mr-1' href={url} target='_blank' rel="noreferrer" download key={name}>
-                        <Icon path={icon} size={1} className='icon mr-2' />{name}
-                    </a>
-                )}
+                <a className='button is-success mr-1' href={"/data/world.geojson"} target='_blank' rel="noreferrer" download key={t('sidebar.geojson')}>
+                    <Icon path={mdiEarth} size={1} className='icon mr-2' />{t('sidebar.geojson')}
+                </a>
                 <hr className='my-3' />
             </div>
         </div>
