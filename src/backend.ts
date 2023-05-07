0496c1670725ff088b03133d04d7c6b58bcc5363
Filip Czaplicki <github@starsep.com>
2023-05-07 11:35:35 +0200

    refactor: NominatimStateData

--- src/backend.ts
+++ src/backend.ts
@@ -1,7 +1,7 @@
 import { fetchNodeData } from "./osm";
 import { DefibrillatorData } from "./model/defibrillatorData";
 import { Country } from "./model/country";
-import { NominatimData } from "./model/Nominatim";
+import { NominatimStateData } from "./model/Nominatim";
 
 export const backendBaseUrl = process.env.REACT_APP_BACKEND_API_URL;
 
@@ -33,13 +33,25 @@ export async function fetchCountriesData(): Promise<Array<Country> | null> {
             return null;
         });
 }
-
-export async function getNominatimDataForLatLon(lat: number, lon: number): Promise<NominatimData | null> {
+/*
+   Uses Nominatim reverse geocoding API.
+   Returns centroid of the state where AED is located.
+   TODO(openstreetmap-polska/openaedmap-backend#11): move timezone calculation to the backend
+*/
+export async function getNominatimReverseGeocodingState(lat: number, lon: number)
+    : Promise<NominatimStateData | null> {
     try {
         const data = await (await fetch(
             `https://nominatim.openstreetmap.org/reverse?format=json&lon=${lon}&lat=${lat}&zoom=5&addressdetails=1`,
         )).json();
-        return data;
+        return {
+            lat: data.lat,
+            lon: data.lon,
+            address: {
+                country_code: data.address.country_code,
+                state: data.address.state,
+            },
+        };
     } catch (error) {
         console.error("Error:", error);
         throw new Error("Failed to fetch data from API");
--- src/model/Nominatim.ts
+++ src/model/Nominatim.ts
@@ -1,16 +1,3 @@
-export interface NominatimData {
-    "place_id": number,
-    "licence":"Data © OpenStreetMap contributors, ODbL 1.0. https://osm.org/copyright",
-    "osm_type": string,
-    "osm_id":number,
-    "lat": number,
-    "lon": number,
-    "display_name": string,
-    "address": {
-        "state": string,
-        "ISO3166-2-lvl3": string,
-        "country": string,
-        "country_code": string
-    },
-    "boundingbox": [number, number, number, number]
-}
+import { nominatim_object } from "opening_hours";
+
+export type NominatimStateData = nominatim_object;
