d7e82fd79e7ae09d05211c5202f4655a89c9dac7
Shubham Sharma <shubhamku044@gmail.com>
2023-08-21 00:43:23 +0530

    made the logo clickable

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -18,6 +18,7 @@ const SiteNavbar: FC<SiteNavbarProps> = ({ toggleSidebarShown }) => {
         <Navbar color="success" className="has-background-green">
             <Navbar.Brand>
                 <Navbar.Item renderAs="div" pr={1}>
+                  <a href="/">
                     <svg className="navbarLogo" viewBox="0 0 60 60" role="presentation">
                         {/* TODO: extract svg logo */}
                         {/* eslint-disable-next-line max-len */}
@@ -28,6 +29,7 @@ const SiteNavbar: FC<SiteNavbarProps> = ({ toggleSidebarShown }) => {
                         <span className="has-text-weight-semibold">AED</span>
                         Map
                     </span>
+                  </a>
                 </Navbar.Item>
                 <Navbar.Item className="is-hidden-touch" renderAs="div" pl={1} pr={1}>
                     <span className="has-text-weight-light is-size-6 pl-0">
