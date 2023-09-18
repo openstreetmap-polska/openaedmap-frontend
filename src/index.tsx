a134c9e2fcab5eefb49ecb7a5964b7d46ecebdf4
tomasz t <tomasztaras@outlook.com>
2023-09-18 09:23:57 +0000

    updated web vitals package

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import Main from "./Main";
 import "./i18n";
+import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
 root.render(
@@ -10,3 +11,5 @@ root.render(
         <Main />
     </React.StrictMode>,
 );
+
+reportWebVitals(console.log);
