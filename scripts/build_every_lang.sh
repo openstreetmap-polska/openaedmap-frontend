6aeca286c5179b6620f46cddef37d02f27d4dcb3
Filip Czaplicki <github@starsep.com>
2024-02-08 20:47:01 +0100

    Actually build for every language
    LANG_PLACEHOLDER hack was fast but well... hack
    For dev main build is copied faster

--- scripts/build_every_lang.sh
+++ scripts/build_every_lang.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 set -eu
-LANG_PLACEHOLDER=LANG_PLACEHOLDER
-VITE_DEFAULT_LANG=$LANG_PLACEHOLDER
-npm run build -- --outDir build/$VITE_DEFAULT_LANG --base "/$VITE_DEFAULT_LANG"
 
 for lang in public/locales/*; do
     lang=$(basename "$lang")
     if [[ $lang = "debug" ]]; then
         continue
     fi
+    export VITE_DEFAULT_LANG=$lang
     echo "Generating $lang"
-    cp -r "build/$LANG_PLACEHOLDER" "build/$lang"
-    sed -i -- "s/$LANG_PLACEHOLDER/$lang/g" "build/$lang/index.html" "build/$lang/assets"/*
+    npm run build -- --outDir "build/$lang" --base "/$lang"
 done
-rm -rf "build/$LANG_PLACEHOLDER"
