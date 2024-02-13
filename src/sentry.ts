60620b91d3be04f1f4e1d3b37d587bcfe8b3eedd
Filip Czaplicki <github@starsep.com>
2024-02-13 21:29:05 +0100

    Disable performance monitoring
    Fixes #203

--- src/sentry.ts
+++ src/sentry.ts
@@ -4,14 +4,7 @@ export default function initSentry() {
 	if (shouldEnableSentry()) {
 		Sentry.init({
 			dsn: "https://4499d52ff3734be1b204d9f13353ae4c@glitch.openaedmap.org/2",
-			integrations: [Sentry.browserTracingIntegration()],
-			// Performance Monitoring
-			tracesSampleRate: 1.0, //  Capture 100% of the transactions
-			// Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-			tracePropagationTargets: [
-				"localhost:5173",
-				/^https:\/\/(dev.)?openaedmap\.org/,
-			],
+			integrations: [],
 		});
 	}
 }
