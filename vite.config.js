2131565553462c9dc1562422459335b91aa74c82
Kamil Monicz <kamil@monicz.dev>
2024-02-14 07:45:40 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,6 @@
+import path from "path";
 import { sentryVitePlugin } from "@sentry/vite-plugin";
 import react from "@vitejs/plugin-react";
-import path from "path";
 import { defineConfig, loadEnv } from "vite";
 import Sitemap from "vite-plugin-sitemap";
 import languages from "./src/languages";
