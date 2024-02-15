ed7909a7911a7bef14f0e40ea372a81636e9d8b7
Kamil Monicz <kamil@monicz.dev>
2024-02-15 17:26:41 +0100

    Propagate traces to backend requests

--- src/sentry.ts
+++ src/sentry.ts
@@ -1,10 +1,13 @@
 import * as Sentry from "@sentry/react";
+import { backendBaseUrl } from "./backend";
 
 export default function initSentry() {
 	if (shouldEnableSentry()) {
 		Sentry.init({
 			dsn: "https://df77ee799280152a4bc7b88a7a9ee6f4@sentry.openaedmap.org/2",
-			integrations: [],
+			integrations: [Sentry.browserTracingIntegration()],
+			tracesSampleRate: 0,
+			tracePropagationTargets: [backendBaseUrl],
 			environment: import.meta.env.VITE_ENV,
 		});
 	}
